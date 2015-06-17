c637e142f2525922687b73dfe728f98422e3f164
Kim Kiogora <kimkiogora@gmail.com>
2015-06-17 14:51:31 +0300

    Create Singleton.php

---
+++ Singleton.php
@@ -0,0 +1,11 @@
+<?php
+$check_file="Singleton.php";
+$cmd="ps aux | grep -i $check_file | grep -v \"grep\"|awk '{print $2}'";
+$result =array();
+exec($cmd,$result);
+
+if(isset($result[0]) && (int)$result[0]> 0){
+        echo "\nFound another instance\n";
+        exit(1);
+}
+echo "\nNo ther instance found.Proceed\n";
