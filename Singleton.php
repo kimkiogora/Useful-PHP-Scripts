b008a9e3a07325e731d1633aab967df7c46a6623
Kim Kiogora <kimkiogora@gmail.com>
2015-06-17 14:54:29 +0300

    Update Singleton.php

--- Singleton.php
+++ Singleton.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * @author: kim kiogora <kimkiogora@gmail.com>
+ * @notes
+ *      The value of $check_file, should respresent a script that is currently running.
+ *      This can be used for example to prevent a crontab from starting too many processes of the same file.
 */ 
 $check_file="Singleton.php";
 $cmd="ps aux | grep -i $check_file | grep -v \"grep\"|awk '{print $2}'";
