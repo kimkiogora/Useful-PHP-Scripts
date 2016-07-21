aefecbf0924b8f1729cc5ae56e225e6d64148a22
Kim Kiogora <kimkiogora@gmail.com>
2016-07-21 16:30:36 +0300

    Update configs.php

--- configs.php
+++ configs.php
@@ -1,12 +1,12 @@
 <?php
-$main_url = "http://localhost/safindex.php?msisdn=254736465333";
+$main_url = "http://localhost/ussd_index.php?msisdn=254736465333";
 
 $shortcodes = array(
-	"*275#"=>"$main_url&service_code=275&",
-	"*778#"=>"$main_url&service_code=778&",
-	"*699#"=>"$main_url&service_code=699&",
+	"*xxx#"=>"$main_url&service_code=275&",
+	"*yyy#"=>"$main_url&service_code=778&",
+	"*zzz#"=>"$main_url&service_code=699&",
 );
 
 $mnoregex = array(
-	"(^2547)(11){1}[0-9]{6}"=>"safaricom"
+	"(^2547)(11){1}[0-9]{6}"=>"mno_y"
 );
